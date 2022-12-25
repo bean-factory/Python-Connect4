eb6c48b14c3ad2fd509ec2413727eec3ef92b23e
Mridul Agarwal <mridulagarwal160906@gmail.com>
2022-12-25 12:36:11 +0530

    Cleanup

--- Connect4.py
+++ Connect4.py
@@ -1,29 +1,28 @@
-Players = ["Yellow Player's turn","Red Player's turn"]
-
+Players = ["Yellow","Red"]
+L = []
+InProgress = True
 #True for Red, False for Yellow
 Player = True
 
 #ANSI codes for colored output
-Red = '\033[91m'
-End = '\033[0m'
-Yellow = '\33[93m'
+Red = "\033[91m"
+End = "\033[0m"
+Yellow = "\33[93m"
 R = Red+"⬤"+End
 Y = Yellow+"⬤"+End
 
 while True:
-
     def init():
         #Setup board
-        global Height,Height,L
-        
+        print("Rules:","1. Tokens will pile up from the bottom as you place them from the top.", "2. Only 1 move is allowed per turn, and only 2 players can play. Red Player moves first, followed by the Yellow Player", "3. First to connect 4 tokens horizontally or vertically wins.", "4. When it is your turn, specify the column number from the left. A white dot indicates a vacant space.","5. The current board will appear after each move.", sep = "\n") 
         while True:
-            Height = input("Enter width: ")
+            Width = input("Enter width: ")
             try:
-                Height = int(Height)
+                Width = int(Width)
             except ValueError:
                 print("Must be between 4 and 20")
                 continue 
-            if 4 <= Height <= 20:
+            if 4 <= Width <= 20:
                 break
             else:
                 print("Must be between 4 and 20")
@@ -40,11 +39,9 @@ while True:
             else:
                 print("Must be between 4 and 20")
         
-        L = []
-        
         for i in range(Height):
             L2 = []
-            for j in range(Height):
+            for j in range(Width):
                 L2.append("⬤")
             L.append(L2)
     
@@ -56,75 +53,80 @@ while True:
             print()
     
     def check():
-        global L
+        global L,InProgress
         
         #Horizontal Check
         for i in L:
             for j in range(0,len(i)-3):
                 if i[j] == R and i[j+1] == R and i[j+2] == R and i[j+3] == R:
-                    draw()
-                    print("Red has won!")
-                    quit()
+                    win()
                 elif i[j] == Y and i[j+1] == Y and i[j+2] == Y and i[j+3] == Y:
-                    draw()
-                    print("Yellow has won!")
-                    quit()
-
+                    win()
+    
         #Vertical Check
         for i in range(len(L)-3):
             for j in range(0,len(L[i])):
                 if L[i][j] == R and L[i+1][j] == R and L[i+2][j] == R and L[i+3][j] == R:
-                    draw()
-                    print("Red has won! ")
-                    quit()
+                    win()
                 elif L[i][j] == Y and L[i+1][j] == Y and L[i+2][j] == Y and L[i+3][j] == Y:
-                    draw()
-                    print("Yellow has won! ")
-                    quit()
+                    win()
         
         #Draw Check
         if sum(i.count("⬤") for i in L) == 0:
             draw()
             print("Draw!")
-            quit()
-
+            InProgress = False
+    
+    def win():
+        #Display Outcome
+        global InProgress
+        draw()
+        print(Players[int(Player)], "has won!")
+        InProgress = False
+    
     def move():
         #Get Player move
         global Player, Players
-
+            
+        print("-"*25,Players[int(Player)],"Player's Turn","-"*25)
+        C = R if Player == True else Y
+        draw()
+        
         while True:
             
-            print("-"*25,Players[int(Player)],"-"*25)
-            C = R if Player == True else Y
-            draw()
+            N = input("Enter column: ")
             
-            while True:
-                
-                N = input('Enter column: ')
-                
-                try:
-                    N = int(N)
-                except ValueError:
-                    print("Illegal move, please try again.")
-                    print("-"*25,Players[int(Player)],"-"*25)
-                    draw()
-                    continue
-                if 1 <= N <= len(L[0]):
-                    break
-                else:
-                    print("Illegal move, please try again.")
-                    print("-"*25,Players[int(Player)],"-"*25)
-                    draw()
-            
-            for i in range(len(L)-1,-1,-1):
-                if L[i][N-1] == "⬤":
-                    L[i][N-1] = C
-                    Player = not Player
-                    check()
-                    break
+            try:
+                N = int(N)
+            except ValueError:
+                print("Illegal move, please try again.")
+                print("-"*25,Players[int(Player)],"Player's Turn","-"*25)
+                draw()
+                continue
+    
+            if 1 <= N <= len(L[0]):
+                break
             else:
                 print("Illegal move, please try again.")
-
+                print("-"*25,Players[int(Player)],"Player's Turn","-"*25)
+                draw()
+        
+        for i in range(len(L)-1,-1,-1):
+            if L[i][N-1] == "⬤":
+                L[i][N-1] = C
+                check()
+                Player = not Player
+                break
+        else:
+            print("Illegal move, please try again.")
+    
     #Start game
     init()
-    move()
+    while InProgress == True:
+        move()
+    while True:
+        N = input("New game? (Y/N): ").upper()
+        if N == "Y":
+            break
+        elif N == "N":
+            quit()
